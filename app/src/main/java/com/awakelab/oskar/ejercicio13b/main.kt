d471ccbd7f0683f7b43d7c782efbfc068f1f9472
OskarUno <oorellana80@gmail.com>
2023-07-14 22:13:12 -0400

    Ejercicio 13B

---
+++ app/src/main/java/com/awakelab/oskar/ejercicio13b/main.kt
@@ -0,0 +1,42 @@
+package com.awakelab.oskar.ejercicio13b
+
+data class Heroe(
+    val superHero: String,
+    val publisher: String,
+    val realName: String,
+    val photo: String,
+
+    )
+
+fun main(){
+    //  var i = 0
+    println("Ingrese super Heroes, para salir presione   ")
+    // var cant:Int = readln().toInt()
+    var heroes = mutableListOf<Heroe>()
+    var cant : Int= 1;
+    while (cant == 1) {
+        print("Nombre: ")
+        var superHero = readln()
+
+        print("publisher: ")
+        var publisher = readln()
+
+        print("realName: ")
+        var realName = readln()
+
+        print("photo: ")
+        var photo = readln()
+        heroes.add(Heroe(superHero,publisher,realName,photo))
+
+        print("Para Salir Presiones 'S' o 's' ")
+        val salirContuar = readln()
+        if(salirContuar == "S" || salirContuar == "s"){
+            cant = 0
+        }
+        // i++
+    }
+
+    for (h in heroes) {
+        println(h)
+    }
+}
